fd2c8f5a6136f6f42f17cfaf7de6b499344f4993
richa <richa1998.patel@gmail.com>
2020-01-24 18:35:32 -0500

    Initial Commit

---
+++ 764947_Richa_FinalLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  764947_Richa_FinalLab
+//
+//  Created by Richa Patel on 2020-01-24.
+//  Copyright © 2020 Richa Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
